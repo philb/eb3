b1e9c2e990306c0598b3ac90e13b00855b68032f
Phil Blundell <philb@brightsign.biz>
2021-03-30 17:24:15 +0100

    spi_master: Add big comment

--- fpga/rtl/spi_master.v
+++ fpga/rtl/spi_master.v
@@ -1,3 +1,38 @@
+// Move data between the CPU and transmit/receive blocks using SPI
+//
+// Port naming in this module is from the perspective of the CPU, 
+// so "rx_data" is data coming out of the receiver and going to
+// the CPU.
+//
+// The receiver (rx_top) places 16 bits of parallel data on rx_data[]
+// and then drives rx_strobe high to indicate it has something to
+// send.  When the SPI block is ready to transfer, it latches rx_data[]
+// into its internal shift register and drives rx_accept high to
+// indicate that the data has been latched.  rx_accept will stay high
+// for one SCK cycle.  We do not wait for the receiver to deassert
+// rx_strobe, we assume it will do so at some point during the SPI
+// transmit cycle.
+//
+// The transmitter (tx_top) drives tx_request high to indicate it
+// wants input data from the CPU.  SPI moves 16 bits from the CPU into
+// the tx shift register then asserts tx_strobe to indicate that data
+// is ready.  It then blocks waiting to see tx_request deasserted
+// before it will accept another request from either transmitter or
+// receiver.  If the transmitter wants to start another transfer then
+// it must deassert tx_request, wait for tx_strobe to go low, and then
+// assert tx_request again for the next operation.
+//
+// We use SPI mode 0: data changes on falling edge of SCK, latched on
+// rising edge of SCK, first data bit moves on the first rising edge
+// of SCK following SS# asserted.
+//
+// We will always deassert SS for at least two SCK cycles between
+// transfers so a transfer takes a minimum of 18 SCK cycles (= 36 MCLK
+// cycles).  The 16-bit payload is 8 bits of data plus 8 bits of
+// status so we actually end up moving about 1 payload bit per 5 MCLK
+// cycles on average.  This is still fine because the 25MHz MCLK is
+// at least an order of magnitude faster than the ~500kHz line clock.
+
 module spi_master(clk, reset, rx_data, rx_strobe, rx_accept, tx_request, tx_data, tx_strobe, ss, sck, mosi, miso);
 
    input clk;
