635dd72b89c69df1a9fa8fb83eb4b04daac18f42
Phil Blundell <philb@brightsign.biz>
2021-04-01 08:02:43 +0100

    tx_framer: Remove stray port width

--- fpga/rtl/tx_framer.v
+++ fpga/rtl/tx_framer.v
@@ -1,4 +1,4 @@
-module tx_framer(netclk, reset, txdata, flag_fill, data_in[7:0], data_available, data_consumed, eop, underrun);
+module tx_framer(netclk, reset, txdata, flag_fill, data_in, data_available, data_consumed, eop, underrun);
 
    input         netclk;
    input 	 reset;
