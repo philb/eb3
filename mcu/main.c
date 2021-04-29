91ffca621c5acfb554705625d715074390b07f4f
Phil Blundell <philb@brightsign.biz>
2021-04-29 23:07:21 +0100

    mcu: Receive frame reporting works

--- mcu/main.c
+++ mcu/main.c
@@ -4,16 +4,33 @@
 #define PB03 GPIO(GPIO_PORTB, 3)
 #define PB05 GPIO(GPIO_PORTB, 5)
 #define PB14 GPIO(GPIO_PORTB, 14)
+#define PB15 GPIO(GPIO_PORTB, 15)
+#define PB16 GPIO(GPIO_PORTB, 16)
+#define PB17 GPIO(GPIO_PORTB, 17)
+#define PB18 GPIO(GPIO_PORTB, 18)
+#define PB19 GPIO(GPIO_PORTB, 19)
 #define PB20 GPIO(GPIO_PORTB, 20)
 #define PB21 GPIO(GPIO_PORTB, 21)
 
 #define PA02 GPIO(GPIO_PORTA, 2)
 #define PA14 GPIO(GPIO_PORTA, 14)
 #define PA16 GPIO(GPIO_PORTA, 16)
+#define PA20 GPIO(GPIO_PORTA, 20)
+#define PA21 GPIO(GPIO_PORTA, 21)
+#define PA22 GPIO(GPIO_PORTA, 22)
 
 #define PC00 GPIO(GPIO_PORTC, 0)
 #define PC01 GPIO(GPIO_PORTC, 1)
 #define PC02 GPIO(GPIO_PORTC, 2)
+#define PC24 GPIO(GPIO_PORTC, 24)
+
+#define FPGA_SOFT_RESET	PB16
+#define FPGA_RX_ENABLE  PB18
+#define FPGA_TX_GO      PB19
+#define FPGA_FLAG_FILL  PA22
+
+#define FPGA_IDLE	PA20
+#define FPGA_NO_CLOCK	PA21
 
 #define LED_SD		PA02
 #define LED_ECONET	PA01
@@ -145,9 +162,6 @@ int main(void)
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
 
-	usb_d_enable();
-	//usb_d_attach();
-
 	// CLOCKOUT_EN
 	gpio_set_pin_level(PB03, false);
 	gpio_set_pin_direction(PB03, GPIO_DIRECTION_OUT);
@@ -181,13 +195,71 @@ int main(void)
 	gpio_set_pin_function(PA14, PINMUX_PA14M_GCLK_IO0);
 	gpio_set_pin_pull_mode(PA14, GPIO_PULL_OFF);
 
+	// FPGA soft reset
+	gpio_set_pin_function(FPGA_SOFT_RESET, 0);
+	gpio_set_pin_level(FPGA_SOFT_RESET, 1);
+	gpio_set_pin_direction(FPGA_SOFT_RESET, GPIO_DIRECTION_OUT);
+
+	// FPGA rx enable
+	gpio_set_pin_function(FPGA_RX_ENABLE, 0);
+	gpio_set_pin_level(FPGA_RX_ENABLE, 0);
+	gpio_set_pin_direction(FPGA_RX_ENABLE, GPIO_DIRECTION_OUT);
+
+	// flag fill
+	gpio_set_pin_function(FPGA_FLAG_FILL, 0);
+	gpio_set_pin_level(FPGA_FLAG_FILL, 0);
+	gpio_set_pin_direction(FPGA_FLAG_FILL, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(FPGA_TX_GO, 0);
+	gpio_set_pin_level(FPGA_TX_GO, 0);
+	gpio_set_pin_direction(FPGA_TX_GO, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(FPGA_NO_CLOCK, 0);
+	gpio_set_pin_direction(FPGA_NO_CLOCK, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_function(FPGA_IDLE, 0);
+	gpio_set_pin_direction(FPGA_IDLE, GPIO_DIRECTION_IN);
+
 	lamp_test();
 
 	gpio_set_pin_level(PB05, true);
 
 	load_fpga();
 
-	/* Replace with your application code */
+	//vendor_example();
+
+	gpio_set_pin_level(FPGA_SOFT_RESET, 0);
+
+	spi_s_sync_enable(&SPI_1);
+
+	gpio_set_pin_level(FPGA_RX_ENABLE, 1);
+
+	//gpio_set_pin_direction(PC24, GPIO_DIRECTION_IN);
+
+#if 0
 	while (1) {
+		if (gpio_get_pin_level(FPGA_NO_CLOCK))
+			debug_putc('C');
+		if (gpio_get_pin_level(FPGA_IDLE) == 0)
+			debug_putc('I');
+		if (gpio_get_pin_level(PC24) == 0)
+			debug_putc('S');
+	}
+#endif
+
+	while (1) {
+		struct spi_xfer msg;
+		uint8_t buf[32];
+		memset(&msg, 0, sizeof(msg));
+		msg.txbuf = NULL;
+		msg.rxbuf = buf;
+		msg.size = sizeof(buf);
+		int n = spi_s_sync_transfer(&SPI_1, &msg);
+		debug_puts("Rx ");
+		debug_put_hex(n);
+		debug_putc(' ');
+		for (int i = 0; i < n; i++)
+			debug_put_hex(buf[i]);
+		debug_putc('\n');
 	}
 }
